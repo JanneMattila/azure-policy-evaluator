e5e33e0f139399325a81c24d352fd578f54edd4d
Janne Mattila <janne.mattila@hotmail.fi>
2023-12-09 21:23:15 +0200

    Add CLI dev capability

--- src/AzurePolicyEvaluator/Program.cs
+++ src/AzurePolicyEvaluator/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AzurePolicyEvaluator;
+
+Console.WriteLine("Azure Policy Evaluator");
+
+var policy = File.ReadAllText(args[0]);
+var test = File.ReadAllText(args[1]);
+
+var evaluator = new Evaluator();
+var evaluationResult = evaluator.Evaluate(policy, test);
+Console.WriteLine(evaluationResult.IsSuccess);
+
